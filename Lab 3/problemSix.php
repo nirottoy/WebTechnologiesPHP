6f25f9339b4b2460ec9744a6549293a8b065d185
Saha Junior Samay <nirottoy@gmail.com>
2021-02-08 13:23:05 +0600

    Probelm Six as 2nd Files 01 uploaded

---
+++ Lab 3/problemSix.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Problem Six</h1>
+
+<?php
+$color = array('white', 'green', 'red', 'blue', 'black');
+echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the $color[0] carpet, the $color[1] lawn, the $color[0] house, the leaden sky. The new president and his first lady. - Richard M. Nixon";
+?>
+
+
+</body>
+</html>
